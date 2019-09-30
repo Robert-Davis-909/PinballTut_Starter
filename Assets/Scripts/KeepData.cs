9300f5a27cd2635a4001f6a110951de62a82c836
Robert-Davis-909 <54360358+Robert-Davis-909@users.noreply.github.com>
2019-09-30 15:09:29 -0500

    Initial commit

---
+++ Assets/Scripts/KeepData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeepData : MonoBehaviour
+{
+    public Text showScore;
+    public Text HighScore;
+  
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void ShowScore()
+    {
+        showScore.text = DestroyBall.aScore.ToString();
+        Debug.Log(DestroyBall.HighScore);
+    }
+
+    public void ShowHighScore()
+    {
+        HighScore.text = DestroyBall.HighScore.ToString();
+        Debug.Log(DestroyBall.HighScore);
+    }
+
+}
---
+++ Assets/Scripts/NextEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextEvent : MonoBehaviour
+{
+    public void Instructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void quitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+    
+    public void exitGame()
+    {
+        SceneManager.LoadScene("Exit");
+    }
+    public void Starter_Scene()
+    {
+        SceneManager.LoadScene("Starter Scene");
+    }
+
+    public void GoBacktoInstructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+}
